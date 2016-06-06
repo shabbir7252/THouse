8760716003400ecc5823f20ec1d57dd6b8cf3191
Shabbir Salumberwala <ssalumberwala@psc.com.kw>
2016-06-06 14:26:39 +0300

    added suggest page and partial page

---
+++ Inc/footer.php
@@ -0,0 +1,18 @@
+</div> <!--content End-->
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
+				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Website</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,33 +1,4 @@
-
-<html>
-<head>
-	<title>
-		<?php
-			echo "Personal Media Library";
-		?>
-	</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
-
-			<ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
-            </ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
+<?php include("inc/header.php"); ?>
 		<div class="section catalog random">
 
 			<div class="wrapper">
@@ -42,22 +13,4 @@
 
 		</div>
 
-	</div>
-
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>		
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy;<?php echo date("Y"); ?> Personal Media Website</p>
-
-		</div>
-	
-	</div>
-
-</body>
-</html>
+<?php include("inc/footer.php"); ?>
