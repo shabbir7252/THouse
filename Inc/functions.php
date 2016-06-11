ee8cc0afe3f9e37e01bfcdb8925790e1c90bf06d
Shabbir7252 <shabbir7252@live.com>
2016-06-11 21:41:35 +0300

    Added form

--- Inc/functions.php
+++ Inc/functions.php
@@ -1,6 +1,6 @@
 <?php
 function get_item_html($id,$item){
- $output = "<li><a href='#'><img src='" 
+ $output = "<li><a href='details.php?id=".$id."'><img src='" 
                     . $item["img"] . "' alt='"
                     . $item["title"] . "' />" 
                     . "<p>View Details</p>" 
--- suggest.php
+++ suggest.php
@@ -6,7 +6,27 @@
 
 
 <div class="section page">
+<div class="wrapper">
     <h1>Suggest a Media Item</h1>
+    <p>If you think there is something I&rsquo;m missing, let me know! complete the form to send me an email.</p>
+    <form method="post">
+        <table>
+            <tr>
+                <th><label for="name">Name </label></th>
+                <td><input id="name" type="text" name="name"></td>
+            </tr>
+            <tr>
+                <th><label for="email">Email </label></th>
+                <td><input id="email" type="text" name="email"></td>
+            </tr>
+            <tr>
+                <th><label for="details">Suggest Item Details </label></th>
+                <td><textarea name="details" id="details" cols="30" rows="10"></textarea></td>
+            </tr>
+        </table>
+        <input type="submit" value="Send" />
+    </form>
+</div>
 </div>
 
 
