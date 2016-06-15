c9788f509221ade12908ce0209f20fa349525943
Shabbir7252 <shabbir7252@live.com>
2016-06-15 22:18:54 +0300

    Added Structure

--- suggest.php
+++ suggest.php
@@ -1,4 +1,26 @@
 <?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $details = $_POST["details"];
+
+    $emailmessage = "";
+    $emailmessage .= "Name : " . $name . "</br>";
+    $emailmessage .= "Email : " . $email . "</br>";
+    $emailmessage .= "Details of message : " . $details;
+
+    echo "<pre>";
+    echo $emailmessage;
+    echo "</pre>";
+
+    //To Do: Send Email
+    header("location:suggest.php?status=thanks");
+}
+?>
+
+
+<?php
     $pageTitle = "Suggested a Media Item";
     $section = "suggest";
     include("inc/header.php");
@@ -8,8 +30,15 @@
 <div class="section page">
 <div class="wrapper">
     <h1>Suggest a Media Item</h1>
+
+        <?php
+        if(isset($_GET["status"]) && $_GET["status"] == "thanks"){
+            echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
+        } else { ?>
+
+    
     <p>If you think there is something I&rsquo;m missing, let me know! complete the form to send me an email.</p>
-    <form method="post">
+    <form method="post" action="suggest.php">
         <table>
             <tr>
                 <th><label for="name">Name </label></th>
@@ -26,6 +55,7 @@
         </table>
         <input type="submit" value="Send" />
     </form>
+    <?php } ?>
 </div>
 </div>
 
